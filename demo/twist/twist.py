63adb13ae70912c5a92e3d073a5ed2110f39da75
Marek Netusil <marek.netusil@gmail.com>
2015-12-09 11:01:55 +0100

    Twist: Adjusted twist.py demo

--- demo/twist/twist.py
+++ demo/twist/twist.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright (C) 2009 Simula Research Laboratory and %s" % __autho
 __license__  = "GNU GPL Version 3 or any later version"
 
 from cbc.twist import *
+from sys import argv
 
 class Twist(StaticHyperelasticity):
 
@@ -12,16 +13,26 @@ class Twist(StaticHyperelasticity):
 
     def dirichlet_values(self):
         clamp = Expression(("0.0", "0.0", "0.0"))
-        twist = Expression(("0.0",
+        """twist = Expression(("0.0",
                             "y0 + (x[1] - y0) * cos(theta) - (x[2] - z0) * sin(theta) - x[1]",
                             "z0 + (x[1] - y0) * sin(theta) + (x[2] - z0) * cos(theta) - x[2]"),
                            y0=0.5, z0=0.5, theta=pi/3)
-        return [clamp, twist]
+        return [clamp, twist]"""
+        return [clamp]
 
     def dirichlet_boundaries(self):
         left = "x[0] == 0.0"
+        #right = "x[0] == 1.0"
+        #return [left, right]
+        return [left]
+
+    def neumann_conditions(self):
+        traction = Constant((float(argv[1]),0.0,0.0))
+        return [traction]
+
+    def neumann_boundaries(self):
         right = "x[0] == 1.0"
-        return [left, right]
+        return [right]
 
     def body_force(self):
         B = Expression(("0.0", "0.0", "0.0"))
@@ -30,20 +41,23 @@ class Twist(StaticHyperelasticity):
     def material_model(self):
         # Material parameters can either be numbers or spatially
         # varying fields. For example,
-        mu       = 3.8461
-        lmbda    = Expression("x[0]*5.8 + (1 - x[0])*5.7")
+        mu       = 1e2
+        lmbda    = 1e5
         C10 = 0.171; C01 = 4.89e-3; C20 = -2.4e-4; C30 = 5.e-4
 
         # It is also easy to switch material models. Uncomment one of
         # the following lines to see a particular material's response.
-        #material = MooneyRivlin([mu/2, mu/2])
-        material = StVenantKirchhoff([mu, lmbda])
-        #material = neoHookean([mu])
-        #material = Isihara([C10, C01, C20])
-        #material = Biderman([C10, C01, C20, C30])
-        #material = Ogden([1.3, 5.0, -2.0, 6.3e5, 0.012e5, -0.1e5])
-
-        return material
+        materials = []
+        materials.append(MooneyRivlin([mu/2, mu/2, lmbda]))
+        materials.append(StVenantKirchhoff([mu, lmbda]))
+        materials.append(neoHookean([mu, lmbda]))
+        materials.append(Isihara([C10, C01, C20, lmbda]))
+        materials.append(Biderman([C10, C01, C20, C30, lmbda]))
+        materials.append(Ogden([1.3, 5.0, -2.0, 6.3e5, 0.012e5, -0.1e5]))
+        
+        index = int(argv[2])
+        print str(materials[index])
+        return materials[index]
 
     def name_method(self, method):
         self.method = method
@@ -57,14 +71,19 @@ class Twist(StaticHyperelasticity):
 twist_dis = Twist()
 twist_dis.name_method("DISPLACEMENT BASED FORMULATION")
 twist_dis.parameters['solver_parameters']['plot_solution'] = False
+twist_dis.parameters['solver_parameters']['element_degree'] = 2
+
+
 twist_up = Twist()
 twist_up.name_method("MIXED (U,P) FORMULATION")
 twist_up.parameters['solver_parameters']['problem_formulation'] = 'mixed_up'
-twist_up.parameters['solver_parameters']['plot_solution'] = False
+twist_up.parameters['solver_parameters']['plot_solution'] = True
+
 
 # Solve the problem
 print twist_dis
 u_dis = twist_dis.solve()
+
 
 print twist_up
 u_up, p_up = twist_up.solve()
@@ -75,3 +94,4 @@ u_e = project(u_up - u_dis,V)
 e = assemble(inner(u_e,u_e)*dx)
 print "L2 norm of the difference between displacement and (u,p) formulation \n ||u_dis - u_up||^2 = %s"  % e
 #plot(u_e, mode = "displacement", interactive=True)
+
